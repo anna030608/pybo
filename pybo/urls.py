8b1c20fe714d36239922cd718fe01ce06d6c757d
anna030608 <anna030608@gmail.com>
2024-06-03 15:54:52 +0900

    변경3

--- pybo/urls.py
+++ pybo/urls.py
@@ -1,16 +1,16 @@
 from django.urls import path
-
-from .views import base_views, question_views, answer_views, comment_views, vote_views
+from pybo.views import base_views, question_views, answer_views, comment_views, vote_views
 
 app_name = 'pybo'
 
 urlpatterns = [
     # base_views.py
-    path('', base_views.index, name='index'),
-    path('<int:question_id>/', base_views.detail, name='detail'),
+    path('question/list/', base_views.index, name='index'),
+    path('question/list/<str:category_name>/', base_views.index, name='index'),
+    path('question/detail/<int:question_id>/', base_views.detail, name='detail'),
 
     # question_views.py
-    path('question/create/', question_views.question_create, name='question_create'),
+    path('question/craete/<str:category_name>/', question_views.question_create, name='question_create'),
     path('question/modify/<int:question_id>/', question_views.question_modify, name='question_modify'),
     path('question/delete/<int:question_id>/', question_views.question_delete, name='question_delete'),
 
@@ -20,9 +20,12 @@ urlpatterns = [
     path('answer/delete/<int:answer_id>/', answer_views.answer_delete, name='answer_delete'),
 
     # comment_views.py
-    path('comment/create/question/<int:question_id>/', comment_views.comment_create_question, name='comment_create_question'),
-    path('comment/modify/question/<int:comment_id>/', comment_views.comment_modify_question, name='comment_modify_question'),
-    path('comment/delete/question/<int:comment_id>/', comment_views.comment_delete_question, name='comment_delete_question'),
+    path('comment/create/question/<int:question_id>/', comment_views.comment_create_question,
+         name='comment_create_question'),
+    path('comment/modify/question/<int:comment_id>/', comment_views.comment_modify_question,
+         name='comment_modify_question'),
+    path('comment/delete/question/<int:comment_id>/', comment_views.comment_delete_question,
+         name='comment_delete_question'),
     path('comment/create/answer/<int:answer_id>/', comment_views.comment_create_answer, name='comment_create_answer'),
     path('comment/modify/answer/<int:comment_id>/', comment_views.comment_modify_answer, name='comment_modify_answer'),
     path('comment/delete/answer/<int:comment_id>/', comment_views.comment_delete_answer, name='comment_delete_answer'),
@@ -30,4 +33,5 @@ urlpatterns = [
     # vote_views.py
     path('vote/question/<int:question_id>/', vote_views.vote_question, name='vote_question'),
     path('vote/answer/<int:answer_id>/', vote_views.vote_answer, name='vote_answer'),
+
 ]
